8eb90c3dfd0782053106a88b97ecc6a0c1b7262f
n-kondou <kon@v7.com>
2025-03-01 08:18:05 +0900

    [update]不要ファイル削除

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -12,9 +12,17 @@ class Food extends Model
     protected $guarded = array('id');
 
     public static $rules = array(
-       // 'user_id' => 'required',
-        'purchase_date' => 'required',
-        'ingreduent' => 'required',
-        'expiration_date' => 'required',
+        'purchase_date' => 'required|date',
+        'ingredient' => 'required|string|max:30',
+        'expiration_date' => 'required|date',
+        'expiration_time' => 'required|integer|min:0|max:24', // 「指定なし」(24) を許可
+        'image' => 'nullable|image|max:2048', // 画像は任意で、最大2MB
+       
+        
     );
+
+    public function histories()
+    {
+        return $this->hasMany('App\Models\History');
+    }
 }
